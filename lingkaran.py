f476f592f97080c0624a46584c2af2fcd148279a
= <=>
2024-10-10 18:31:46 +0700

    first commit

---
+++ lingkaran.py
@@ -0,0 +1,10 @@
+print('='*40)
+print('PROGRAM LINGKARAN')
+print('='*40)
+
+r = float(input('Masukan jari-jari: '))
+l = 3.14*r*r
+k = 2*3.14*r
+
+print('luas:',l, 'cm2')
+print('keliling:',k, 'cm')
