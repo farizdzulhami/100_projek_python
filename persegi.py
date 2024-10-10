7bb393a3a11ebcbb46ba28abc6de882206acb669
= <=>
2024-10-10 15:08:14 +0700

    first commit

--- persegi.py
+++ persegi.py
@@ -1,1 +1,12 @@
-print("program persegi")
+print('='*40)
+print('persegi\t\t: ')
+
+
+print('='*40)
+
+s = int(input('Masukan nilai sisi\t = '))
+l = s*s
+k = 4*s
+
+print('luas persegi adalah = ',l)
+print('keliling persegi adalah = ',k)
