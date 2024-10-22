802a8958a8d6f1d90350b6fc5a72cdeb511346d1
= <=>
2024-10-22 08:41:25 +0700

    first commit

---
+++ jajargenjang.py
@@ -0,0 +1,13 @@
+print('='*40)
+print('PROGRAM JAJAR GENJANG')
+print('='*40)
+
+a = int(input('Alas\t\t: '))
+t = int(input('Tinggi\t\t: '))
+sm = int(input('Sisi Miring\t: '))
+
+l = a*t
+k = 2*(a+sm)
+
+print(f'Luas\t\t: {l}')
+print(f'Keliling\t: {k}')
